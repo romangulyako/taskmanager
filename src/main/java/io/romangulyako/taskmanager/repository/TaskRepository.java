5b484ff7187e051b284338bfec88747cdb9ae495
Roman Gulyako <romangulyako@gmail.com>
2026-01-10 15:55:00 +0300

    feat: add TaskRepository interface

---
+++ src/main/java/io/romangulyako/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package io.romangulyako.taskmanager.repository;
+
+import io.romangulyako.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(Task.Status status);
+}
