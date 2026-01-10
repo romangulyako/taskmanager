33628ff438402ab1c6e9b6be0ec85896fb111c0f
Roman Gulyako <romangulyako@gmail.com>
2026-01-10 15:58:57 +0300

    feat: add TaskController with REST endpoints

---
+++ src/main/java/io/romangulyako/taskmanager/controller/TaskController.java
@@ -0,0 +1,60 @@
+package io.romangulyako.taskmanager.controller;
+
+import io.romangulyako.taskmanager.dto.TaskRequest;
+import io.romangulyako.taskmanager.dto.TaskResponse;
+import io.romangulyako.taskmanager.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(@Valid @RequestBody TaskRequest taskRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(taskService.create(taskRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponse> updateTask(@PathVariable Long id,
+                                                   @Valid @RequestBody TaskRequest taskRequest) {
+        return ResponseEntity.ok(taskService.update(id, taskRequest));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.get(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAll());
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<List<TaskResponse>> getByStatus(@Valid @RequestParam String status) {
+        return ResponseEntity.ok(taskService.getAllByStatus(status));
+    }
+}
